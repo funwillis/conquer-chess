87ad0708b3ff1d37f000a6a60fa03ef6cd59bd9a
funwillis <bmr120591@gmail.com>
2015-11-08 23:52:58 +0100

    Really added HTML plattform
    In the initialization commit did not really added HTML support. Done
    now.

---
+++ html/src/com/calicoco/games/conquerchess/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.calicoco.games.conquerchess.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.calicoco.games.conquerchess.ConquerChess;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new ConquerChess();
+        }
+}
