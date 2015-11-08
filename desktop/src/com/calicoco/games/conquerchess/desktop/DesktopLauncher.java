99a1911110c0a6c2c1647d3b605b1b4a92347447
funwillis <bmr120591@gmail.com>
2015-11-08 23:48:36 +0100

    Repository initialization
    Created project structure using libGDX utility. Enabled Desktop and HTML
    plattforms.

---
+++ desktop/src/com/calicoco/games/conquerchess/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.calicoco.games.conquerchess.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.calicoco.games.conquerchess.ConquerChess;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ConquerChess(), config);
+	}
+}
